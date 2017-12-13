ed28388f81712d5345b4013969fa41b14c06ff3f
Weyert de Boer <weyert.deboer@keytree.co.uk>
2017-12-13 17:30:37 +0100

    Removed the debug logging statements as they are needed anymore Allow requeueing a task with a exponential delay by using a dead letter queue for this we recreate a `retry`-exchange with a retry queue inside it. A message that's being requeued by called `Task#requeueTaskForRetry` will use `Client#retryPublish` to publish a message with a expiry date. If the message is expired it will be forwarded to the death letter exchange which is our normal worker exchange so it can be reprocessed.

--- index.js
+++ index.js
@@ -17,12 +17,10 @@ module.exports = class TaskerTrailpack extends Trailpack {
    * TODO document method
    */
   validate() {
-    this.app.log.debug('trailpackTasker.validate()');
     this.app.config.tasker = _.defaultsDeep(this.app.config.tasker, config.defaults)
     return new Promise((resolve, reject) => {
       joi.validate(this.app.config.tasker, config.schema, (err, value) => {
         if (err) {
-          this.app.log.debug('trailpackTasker.validate() Error: ', err);
           return reject(new Error('Tasker Configuration: ' + err))
         }
 
@@ -35,14 +33,11 @@ module.exports = class TaskerTrailpack extends Trailpack {
    * configure rabbitmq exchanges, queues, bindings and handlers
    */
   configure() {
-    this.app.log.debug('trailpackTasker.configure()');
     let taskerConfig = this.app.config.tasker
-    this.app.log.debug('[1] taskerConfig: ', taskerConfig)
     const profile = getWorkerProfile(taskerConfig)
     taskerConfig = configureExchangesAndQueues(profile, taskerConfig)
-    this.app.log.debug('profile: ', profile)
 
-    this.app.tasker = new Client(this.app, rabbit, taskerConfig.exchangeName)
+    this.app.tasker = new Client(this.app, rabbit, taskerConfig.exchangeName, taskerConfig.retryExchangeName, taskerConfig.maxTaskRetries, taskerConfig.taskRetryDelay)
     TaskerUtils.registerTasks(profile, this.app, rabbit)
 
     this.app.workerCount = 0
@@ -53,19 +48,18 @@ module.exports = class TaskerTrailpack extends Trailpack {
    * Establish connection to the RabbitMQ exchange, listen for tasks.
    */
   initialize() {
-    this.app.log.debug('trailpackTasker.initialize()');
-
     this.app.on('trails:ready', () => {
       return new Promise((resolve, reject) => {
           rabbit.configure(this.app.config.tasker)
-          .then((result => {
-            this.app.log.debug('Connected to Rabbitmq', result)
-            return resolve(true)
-          }))
-          .catch(err => {
-            this.app.log.debug('Failed to connect to the Rabbitmq cluster', err)
-            return resolve(false)
-          })
+            .then((result => {
+              this.app.log.debug('Successfully connected to RabbitMQ server.')
+              return resolve(true)
+            }))
+            .catch(err => {
+              this.app.log.debug('Failed to connect to the RabbitMQ server.')
+              this.app.log.debug('Related error: ', err)
+              return resolve(false)
+            })
       })
     })
   }
@@ -86,7 +80,6 @@ module.exports = class TaskerTrailpack extends Trailpack {
  */
 function getWorkerProfile(taskerConfig) {
   const profileName = taskerConfig.worker
-  console.log('trailpackTasker.getWorkerProfile() profileName: %s', profileName);
 
   if (!profileName || !taskerConfig.profiles[profileName]) {
     return { tasks: [] }
@@ -103,45 +96,70 @@ function getWorkerProfile(taskerConfig) {
  */
 function configureExchangesAndQueues(profile, taskerConfig) {
   const exchangeName = taskerConfig.exchange || 'tasker-work-x'
+  const retryExchangeName = taskerConfig.retryExchange || 'tasker-retry-work-x'
+
   const workQueueName = taskerConfig.workQueueName || 'tasker-work-q'
   const interruptQueueName = taskerConfig.interruptQueueName || 'tasker-interrupt-q'
+  const retryQueueName = taskerConfig.retryQueueName || 'tasker-retry-q'
   const limit = taskerConfig.concurrentTasks || 10
-  console.log('trailpackTasker.configureExchangesAndQueues()', exchangeName, workQueueName, interruptQueueName, limit);
 
   taskerConfig.exchangeName = exchangeName
-  taskerConfig.exchanges = [{
-    name: exchangeName,
-    type: 'topic',
-    autoDelete: false
-  }]
+  taskerConfig.retryExchangeName = retryExchangeName
+  taskerConfig.exchanges = [
+    {
+      name: exchangeName,
+      type: 'topic',
+      autoDelete: false
+    },
+    {
+      name: retryExchangeName,
+      type: 'fanout',
+      autoDelete: false
+    }
+  ]
 
-  taskerConfig.queues = [{
-    name: workQueueName,
-    autoDelete: false,
-    subscribe: true,
-    limit: limit,
-  }, {
-    name: interruptQueueName,
-    autoDelete: false,
-    subscribe: true,
-    limit: limit,
-  }]
+  taskerConfig.queues = [
+    {
+      name: workQueueName,
+      autoDelete: false,
+      subscribe: true,
+      limit: limit,
+    },
+    {
+      name: interruptQueueName,
+      autoDelete: false,
+      subscribe: true,
+      limit: limit,
+    },
+    {
+      name: retryQueueName,
+      deadLetterExchange: exchangeName,
+      noAck: true,
+      autoDelete: false,
+      subscribe: false,
+      limit: limit,
+    }
+  ]
 
-  taskerConfig.bindings = [{
-    exchange: exchangeName,
-    target: workQueueName,
-    keys: profile.tasks
-  }, {
-    exchange: exchangeName,
-    target: interruptQueueName,
-    keys: profile.tasks.map(task => task + '.interrupt')
-  }]
+  taskerConfig.bindings = [
+    {
+      exchange: exchangeName,
+      target: workQueueName,
+      keys: profile.tasks
+    },
+    {
+      exchange: exchangeName,
+      target: interruptQueueName,
+      keys: profile.tasks.map(task => task + '.interrupt')
+    },
+    {
+      exchange: retryExchangeName,
+      target: retryQueueName,
+      keys: profile.tasks.map(task => task + '.retry')
+    },
+  ];
 
   return taskerConfig
 }
-
-
-
-
 
 module.exports.Task = lib.Task
